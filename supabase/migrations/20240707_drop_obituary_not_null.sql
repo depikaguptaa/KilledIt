5e890133350614e11f1ed7aae9588d3a353c434b
Depika Gupta <depikag21@gmail.com>
2025-07-08 00:32:14 +0530

    Added more comps

--- supabase/migrations/20240707_drop_obituary_not_null.sql
+++
@@ -1,5 +0,0 @@
--- Migration: allow NULL obituaryId in Reaction for comment-level likes
-
--- Drop NOT NULL on obituaryId so rows that target a comment only can omit obituaryId
-ALTER TABLE "Reaction"
-  ALTER COLUMN "obituaryId" DROP NOT NULL; 
